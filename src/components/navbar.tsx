88f8d602fa088f63fa1b0153029fd7751669bb0f
mtk30765 <wei.chenglee@mediatek.com>
2025-05-02 16:28:01 +0800

    feat: update site name from ACME to AgentCanvas in Navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -55,7 +55,7 @@ export const Navbar = () => {
             href="/"
           >
             <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <p className="font-bold text-inherit">AgentCanvas</p>
           </Link>
         </NavbarBrand>
         <div className="hidden lg:flex gap-4 justify-start ml-2">
