098130c42b19c81b402b35c9872876404243ddaf
mtk30765 <wei.chenglee@mediatek.com>
2025-05-02 15:10:41 +0800

    feat: add ChatPage component and update API configuration; remove About and Pricing pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 
 import IndexPage from "@/pages/index";
+import ChatPage from "@/pages/chat";
 import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
-import AboutPage from "@/pages/about";
 
 function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
+      <Route element={<ChatPage />} path="/chat" />
+      {/* <Route element={<ChatPage />} path="/chat/:id" /> */}
+      {/* <Route element={<ChatPage />} path="/chat/:id/:title" /> */}
+      {/* <Route element={<ChatPage />} path="/chat/:id/:title/:model" /> */}
+      {/* <Route element={<ChatPage />} path="/chat/:id/:title/:model/:apiType" /> */}
       <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
     </Routes>
   );
 }
