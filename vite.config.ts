5c6a86bd9adacc7eb180b8e76afca9ea2af70f9a
mtk30765 <wei.chenglee@mediatek.com>
2025-05-06 15:15:25 +0800

    refactor: Add allowedHosts configuration to server settings for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    // 允許所有主機
+    allowedHosts: true,
+  },
   preview: {
     // 允許所有主機
     allowedHosts: true,
